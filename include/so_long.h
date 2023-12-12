56e8104e7771a9ea759560e463033ecd03e146e9
Ismael Henrique Da Silva <ishenriq@c2r3p5.42sp.org.br>
2023-12-11 21:26:33 -0300

    começando

---
+++ include/so_long.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 19:42:09 by ishenriq          #+#    #+#             */
+/*   Updated: 2023/12/11 20:23:11 by ishenriq         ###   ########.org.br   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
+# include <math.h>
+# include <MLX42/MLX42.h>
+# include "../lib/libft/libft.h"
+# include "../lib/printf/ft_printf.h"
+
+#endif
