bf729bcd03567ef376a95e64d4309f394c5d9c29
Ismael Henrique Da Silva <ishenriq@c2r3p5.42sp.org.br>
2024-01-31 20:58:36 -0300

    bloqueei a parede 3

--- src/keyhook.c
+++ src/keyhook.c
@@ -6,7 +6,7 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 17:26:44 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/01/31 20:54:16 by ishenriq         ###   ########.org.br   */
+/*   Updated: 2024/01/31 20:58:25 by ishenriq         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,35 +21,26 @@ static int	position_validation(t_main *main, int x, int y)
 
 	j = main->map->x_player;
 	i = main->map->y_player;
-
+	signal = -1;
 	if (x > 0 || y > 0)
 		signal = 1;
-	else
-		signal = -1;
 	if ((x == 0 && y < 0) || (x == 0 && y > 0))
 	{
 		if(!ft_memchr("1", main->map->build_map[i + signal][j], 1))
-		{
 			main->map->y_player = main->map->y_player + signal;
-			return (1);
-		}
 		else
 			return (0);
 	}
 	else if ((x > 0 && y == 0) || (x < 0 && y == 0))
 	{
 		if(!ft_memchr("1", main->map->build_map[i][j + signal], 1))
-		{
 			main->map->x_player = main->map->x_player + signal;
-			return (1);
-		}
 		else
 			return (0);
 	}
 	else
-		return (0);
+		return (1);
 }
-
 
 static void	step(t_main *main, int x, int y)
 {
